01bc5bd76dad2c6a5e86a12f919d4c0ce8ba8f9d
Denis-Moiseev <mrtesla666@gmail.com>
2023-07-11 03:02:32 +0800

    finished

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+double lengs = double.Parse(Console.ReadLine());
+double num = lengs - (lengs % 1);
+int variable = 0;
+Console.Write($"Все чётные числа от 1 до { lengs }: ");
+while (variable < num - 1)
+{
+    variable = variable + 2;
+    Console.Write($"{ variable } ");
+}
